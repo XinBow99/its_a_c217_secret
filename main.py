02bbf3ffd4b98d3ee695bd8c238849cb289e5ab2
Xinbow99 <asdewq45445@gmail.com>
2022-03-30 21:42:39 +0800

    update

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ import numpy as np
 import requests
 
 # 讓猜有哪幾種數字在裡面?
+# 提示：小於五種
 # "1","2",""....?
+# 猜對了才能編過去...
 category_types = []
 # 讀進檔案
 img = cv2.imread("password.jpg")
@@ -19,10 +21,10 @@ for shape in shapes:
     #填充
     points_array = np.array(points,dtype=np.int32)
     mask = cv2.fillPoly(mask, [points_array], category_types.index(category))
-    for c in category_types:
-        if category == c: 
-            mask = cv2.fillPoly(mask, [points_array], 這缺了什麼?)
-        else:
-            mask = cv2.fillPoly(mask,[points_array],category_types.index(category))
+    if category == ...?: 
+        # 顯示？光明的顏色是什麼？閃光？
+        mask = cv2.fillPoly(mask, [points_array], 這要填什麼？)
+    else:
+        mask = cv2.fillPoly(mask,[points_array],category_types.index(category))
 
-cv2.imwrite("test.jpg",mask)
+cv2.imwrite("result.jpg",mask)
